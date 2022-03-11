b7c6447e359d3159be88100aff7d640f2893038e
camillapalombi <camilla.palombi14@gmail.com>
2022-03-11 15:35:02 +0100

    JS Struttura dati

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const app = new Vue ({
+    el : '#root',
+    data : { //struttura dati:
+        arrObjCountries : [
+            {
+                title : 'Svezia',
+                image : '01.jpg',
+                description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                title : 'Svizzera',
+                image : '02.jpg',
+                description : 'Lorem ipsum.'
+            },
+            {
+                title : 'Gran Bretagna',
+                image : '03.jpg',
+                description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title : 'Germania',
+                image : '04.jpg',
+                description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+            },
+            {
+                title : 'Paradise',
+                image : '05.jpg',
+                description : 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+            }
+        ]
+    }
+});
